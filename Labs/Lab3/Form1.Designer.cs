8bff59536998743e8ce3d159b6708093d93aecaa
svetluy <mikitn2009@yandex.ru>
2017-11-30 19:50:54 +0400

    Сделал 3 лабу

---
+++ Labs/Lab3/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(661, 259);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(661, 204);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(46, 22);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(724, 204);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(44, 22);
+            this.numericUpDown2.TabIndex = 2;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 511);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
+
---
+++ Labs/Lab3/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        TextBox[,] boxArr = new TextBox[10, 10];
+        int[,] arr = new int[10, 10];
+
+        public Form1()
+        {
+            InitializeComponent();
+            CreateTable();
+        }
+
+        private void CreateTable()
+        {
+            Point location = new Point(20, 20);
+            Size size = new Size(30, 30);
+
+            foreach (var box in boxArr)
+            {
+                Controls.Remove(box);
+            }
+
+            for (int i = 0; i < numericUpDown1.Value; i++)
+            {
+                for (int j = 0; j < numericUpDown2.Value; j++)
+                {
+                    boxArr[i, j] = new TextBox
+                    {
+                        Location = location,
+                        Size = size
+                    };
+                    boxArr[i, j].Click += textBox_Click;
+                    Controls.Add(boxArr[i,j]);
+                    location.X += 40;
+                }
+                location.X = 20;
+                location.Y += 30;
+            }
+        }
+
+        private void FillTable()
+        {
+            Random random = new Random();
+            for (int i = 0; i < numericUpDown1.Value; i++)
+            {
+                for (int j = 0; j < numericUpDown2.Value; j++)
+                {
+                    arr[i, j] = random.Next(101) - 50;
+                    boxArr[i, j].Text = $"{arr[i, j]}";
+                }
+            }
+        }
+
+        private void textBox_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < numericUpDown1.Value; i++)
+            {
+                for (int j = 0; j < numericUpDown2.Value; j++)
+                {
+                    if (boxArr[i, j].Equals(sender))
+                    {
+                        Add(i,j,arr);
+                    }
+                }
+            }
+        }
+
+        private void Add(int i, int j, int[,] arr)
+        {
+            if(arr[i,j] > 0)
+                for (int k = 0; k < numericUpDown1.Value; k++)
+                {
+                    for (int n = 0; n < numericUpDown2.Value; n++)
+                    {
+                        arr[k, n] += 1;
+                        boxArr[k, n].Text = $"{arr[k,n]}"; 
+                    }
+                }
+            else if(arr[i, j] < 0)
+                for (int k = 0; k < numericUpDown1.Value; k++)
+                {
+                    for (int n = 0; n < numericUpDown2.Value; n++)
+                    {
+                        arr[k, n] -= 1;
+                        boxArr[k, n].Text = $"{arr[k, n]}";
+                    }
+                }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            CreateTable();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FillTable();
+        }
+    }
+}
